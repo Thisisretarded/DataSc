9526b20f87ad0b5df2b355b3a35af3bdd4063438
rt2 <rahul.psit.ec@gmail.com>
2019-08-27 16:14:44 +0530

    changes in Linear Regression script

--- LinearRegression/Linearregression_blog.py
+++ LinearRegression/Linearregression_blog.py
@@ -34,13 +34,14 @@ regressor.fit(X_train,y_train)
 
 y_pred = regressor.predict(X_train)
 
-B0 = regressor.coef_
-B1 = regressor.intercept_
+B1 = regressor.coef_
+B0 = regressor.intercept_
 
+# calculating R2S tatistic value
 xtrainScore = regressor.score(X_test, y_test)*100
 print()
 print()
-print("Eqn is" + " Sales = TV X " + str(B1) +" + " + str(*B0))
+print("Eqn is" + " Sales = TV X " + str(*B1) +" + " + str(*B0))
 print()
 print("Score of prediction: " +  str(xtrainScore))
 #%%
@@ -64,10 +65,6 @@ y_pred = regressor.predict(X_test)
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_test, y_pred, color = 'blue')
 
-for i in range(len(X_test)):
-    lineXdata = (X_test.iloc(0)[i], X_test.iloc(0)[i]) # same X
-    lineYdata = (y_test.iloc(0)[i], y_pred[i]) # different Y
-    plt.plot(lineXdata, lineYdata, color='green')
 #plt.plot(X_train, y_predz, color = 'green')
 plt.grid()
 plt.title('Advt vs. Sales (Test set)')
@@ -75,11 +72,48 @@ plt.xlabel('Advt Budget in $')
 plt.ylabel('Sales in $')
 plt.show()
 
+
+
+# Visualising the Residuals on Test set
+y_pred = regressor.predict(X_test)
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, y_pred, color = 'blue')
+
+for i in range(len(X_test)):
+    lineXdata = (X_test.iloc(0)[i], X_test.iloc(0)[i]) # same X
+    lineYdata = (y_test.iloc(0)[i], y_pred[i]) # different Y
+    plt.plot(lineXdata, lineYdata, color='green', linewidth = 0.5)
+#plt.plot(X_train, y_predz, color = 'green')
+plt.grid()
+plt.title('Advt vs. Sales (Test set)')
+plt.xlabel('Advt Budget in $')
+plt.ylabel('Sales in $')
+plt.show()
+
+
+# Visualising to calculate TSS
+y_pred = regressor.predict(X_test)
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test,[*y_test.mean()]*50, '---')
 #%%
-residual_sum = y_test - y_pred
-plt.scatter(residual_sum**2, y_pred)
+residual_df = y_test - y_pred
+plt.scatter(residual_df**2, y_pred)
 plt.show()
 
 #%%
 y_pred_df = pd.DataFrame(data = np.array([x for a in y_pred for x in a ]))
-print(y_pred_df[0].corr(residual_sum['sales']))
+print(y_pred_df[0].corr(residual_df['sales']))
+
+#%%
+rss = 0
+for x in residual_df['sales']:
+    rss = rss + x**2
+
+tss = 0
+y_avg=y_test.mean()
+total_sum_df = (y_test-y_avg)
+
+for x in total_sum_df['sales']:
+    tss = tss + x**2
+
+#%%
